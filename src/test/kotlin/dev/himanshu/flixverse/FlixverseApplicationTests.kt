d5bfe99d108ebeb6871f87d501efd666c3ce84b6
Himanshu <himanshupat95@gmail.com>
2023-08-07 13:18:01 +0530

    Setting up

---
+++ src/test/kotlin/dev/himanshu/flixverse/FlixverseApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.himanshu.flixverse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlixverseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
