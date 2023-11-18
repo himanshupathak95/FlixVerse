30585b11635e16f3676611453dd3bfad4ab20a0c
Himanshu Pathak <himanshupat95@gmail.com>
2023-11-18 17:05:07 +0530

    -UI improvements

--- client/flixverse/src/components/login-register/Register.jsx
+++ client/flixverse/src/components/login-register/Register.jsx
@@ -18,7 +18,7 @@ export const Register = (props) => {
                 <label htmlFor="name">Full name</label>
                 <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full name" />
                 <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                 <label htmlFor="password"> </label>
